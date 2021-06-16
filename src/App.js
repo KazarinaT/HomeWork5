3a2a4fefeac98a2a3683daeffc6ebcfc8ceadca7
KazarinaT <tetianakazarina@gmail.com>
2021-06-16 15:51:14 +0300

    Home Work5

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
+
+function NameMovie(props){
+  return <h2 className="App1">{props.name}</h2>;
+}
+
+function YearMovie(props){
+  return<h2 className="App2">{props.yearm}</h2>;
+}
+
+
+function Button(props) {
+  const handlerClick = () => {     
+   props.onClickAct(props.bkColor,props.MyColor,props.text);   
+  };
+  return (
+  <button  onClick={handlerClick} 
+  style={{ backgroundColor: props.bkColor,color:props.MyColor }}>
+  {props.text}
+  </button>
+  );
+ }
+ function DisplayBlock(props) {
+   return (
+   <div className='MyBloc'
+   style={{ backgroundColor:props.bkColor ,color:props.MyColor }}>
+   Some text
+   </div>
+   );
+  }
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [displayBkColor,setDisplayBkColor] = useState("white");
+
+  const [MyBkColor,setMyBkColor] = useState("orange");
+  const [myText,setMyText] = useState("Some Text");
+   let stateFunc = (newBkColor,newMyColor, myText) => {
+     setDisplayBkColor(newBkColor);
+       setMyText(myText);
+      setMyBkColor(newMyColor);
+   }
+  return ( 
+  <div className="App">
+    <NameMovie name="Ghost Rider (Призрачный Гонщик)" />
+    <YearMovie yearm="2007"/>
+    <br/>
+    <div>
+    <img className="App-img" src ="./ghost3.jpg" alt="Ghost Rider"/>
+    
+      <span className="App-span">
+      When motorcycle rider Johnny Blaze sells his soul to the Devil to save his father's life,
+      <br/>
+      he is transformed into the Ghost Rider, the Devil's own bounty hunter, and is sent to hunt down sinners.
+      <br/><br/>
+      Director    Mark Steven Johnson(directed by)
+      <br/><br/>
+      Writer  Mark Steven Johnson
+      <br/><br/>
+      Stars Nicolas CageEva MendesSam Elliott
+      <br/>
+      </span>     
+    </div>  
+
+    <Button bkColor="red" MyColor="blue" text="Red" onClickAct={stateFunc}
+ />
+ <Button bkColor="green" MyColor="red" text="Green"
+ onClickAct={stateFunc} />
+ <Button bkColor="yellow" MyColor="green" text="Yellow"
+ onClickAct={stateFunc}/>
+ <DisplayBlock  bkColor={displayBkColor} MyColor={MyBkColor}  text={myText}/>
+    
+  </div>
   );
 }
 
